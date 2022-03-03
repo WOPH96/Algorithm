f1d8f02da6062ff63e907dc514825a656b53978d
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-03-03 19:02:38 +0900

    start

---
+++ build/CMakeFiles/jongmanbook.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jongmanbook.dir/main.cpp.o"
+  "CMakeFiles/jongmanbook.dir/main.cpp.o.d"
+  "jongmanbook"
+  "jongmanbook.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jongmanbook.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
