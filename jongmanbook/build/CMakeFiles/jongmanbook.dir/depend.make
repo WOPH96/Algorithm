71b40e0baab72317f959f6ebd396f64a154628c6
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-05-02 16:34:07 +0900

    .

--- jongmanbook/build/CMakeFiles/jongmanbook.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for jongmanbook.
-# This may be replaced when dependencies are built.
--- jongmanbook/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yang-wonphil/Desktop/study/jongmanbook
-# Build directory: /Users/yang-wonphil/Desktop/study/jongmanbook/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
