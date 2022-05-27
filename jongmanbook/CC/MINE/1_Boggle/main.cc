4ce1fd018ab525e8ba263f19d920b06d951a0f65
wp <ywp0473@nepes.co.kr>
2022-05-27 15:21:57 +0100

    .

--- jongmanbook/CC/MINE/1_Boggle/main.cc
+++ jongmanbook/CC/MINE/1_Boggle/main.cc
@@ -5,16 +5,13 @@ using namespace std;
 
 int main(){
 	
-	string alpha[5][5]={
-		{U,R,L,P,M},
-		{X,P,R,E,T},
-		{G,I,A,E,T},
-		{X,T,N,Z,Y},
-		{X,O,Q,R,S}
+	char alpha[5][5]={
+		{'U','R','L','P','M'},
+		{'X','P','R','E','T'},
+		{'G','I','A','E','T'},
+		{'X','T','N','Z','Y'},
+		{'X','O','Q','R','S'}
 	};
-	for ( const char &elem : alpha){
-		cout << elem << endl;
-	}
 	return 0;
 }
 
